1bd10ce254a6f4312b09ea5d696c999d3d1277c6
ax5495 <axl5495@gmail.com>
2015-12-18 15:58:18 +0100

    doxygene

--- Programmation/E-Market/Modele/Commerce/Produit/Produit.h
+++ Programmation/E-Market/Modele/Commerce/Produit/Produit.h
@@ -8,46 +8,90 @@ class Produit
 {
 private:
     //Fields
+    /// @brief char *  libelle
     char* libelle;
+    /// @brief char *  description
     char* description;
+    /// @brief int idVendeur
     int idVendeur;
+    /// @brief int reference
     int reference;
+    /// @brief int stockDispo
     int stockDispo;
+    /// @brief Vente * vente
     Vente * vente;
+    /// @brief LesTags tags
     LesTags * lesTags;
 
     //Methods
+    /// @brief fonction incrementant NOMBRE_PRODUIT
+    ///        lors de la création d'un produit
     static void incrNB_PROD();
+    /// @brief fonction decrementant NOMBRE_PRODUIT
+    ///        lors de la suppression d'un produit
     static void decrNB_PROD();
 
 public:
     //Static Fields
+    /// @brief int NOMBRE_PRODUIT
+    ///        ce champs represente le nombre de produits
     static unsigned int NOMBRE_PRODUIT;
 
     //Constructor Destructor
+    /// @brief constructeur d'un produit
     Produit();
+    /// @brief constructeur d'un produit
+    /// @param char *
+    /// @param char *
+    /// @param int
+    /// @param int
+    /// @param Vente
     Produit(char*, char*, int, int, Vente&);
+    /// @brief destructeur du produit
     virtual ~Produit();
 
     //Methods
+    /// @brief recuperer le libelle
     char* getLibelle() const;
+    /// @brief recuperer la description
     char* getDescription() const;
+    /// @brief recuperer la reference
     int getReference() const;
+    /// @brief recuperer le stock
     int getStock() const;
+    /// @brief recuperer le type de vente
     Vente * getTypeVente() const;
+    /// @brief verifie si le tag est dans l'objet
+    /// @param char *
     bool isInLesTags(char *);
-
+/// @brief modifier le libelle
+/// @param char *
     void setLibelle(char*);
+    /// @brief modifier la description
+    /// @param char *
     void setDescription(char*);
+    /// @brief modifier la reference
+    /// @param int
     void setReference(int);
+    /// @brief modifier le stock
+    /// @param stock
     void setStockDispo(int);
+    /// @brief modifier le type de vente
+    /// @param Vente *
     void setTypeVente(Vente*);
-
+/// @brief ajouter un tag
+/// @param int
+/// @param ...
     void addTags(int nbArgs, ...);
+    /// @brief supprimer un tag
+    /// @param int
     void rmTag(int);
+    /// @brief supprimer un tag
+    /// @param char *
     void rmTag(char *);
 
     //tempo
+    /// @brief pour le debug des tags
     void afficherTags();
 
 };
--- Programmation/E-Market/Modele/Commerce/Produit/Vente/VenteNormal.h
+++ Programmation/E-Market/Modele/Commerce/Produit/Vente/VenteNormal.h
@@ -3,23 +3,35 @@
 
 #include "Vente.h"
 
+/** @brief La classe VenteNormal permet de créer
+ *         une vente normale.
+ **/
 class VenteNormal : public Vente {
 
 private:
     //Fields
+    /// @brief float prixDeVente
     float prixDeVente;
+    /// @brief char * delaiRetractation
     char * delaiRetractation;
 
 public:
     //Constructor Destructor
+    /// @brief construsteur d'un vente normal
     VenteNormal(float, char *);
+    /// @brief destructeur d'une vente normal
     virtual ~VenteNormal();
 
     //Methods
+    /// @brief recuperer le prix
     virtual float getPrix() const;
+    /// @brief recuperer le délai
     char * getDelai() const;
-
+    /// @brief modifier le prix
+    /// @param float
     virtual void setPrix(float);
+    /// @brief modifier le delais
+    /// @param char *
     void setDelai(char *);
 };
 
